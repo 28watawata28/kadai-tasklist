15d0dcb2d148228c1445f375190ecdfe631a6e2f
watanabe <aasjai>
2018-05-31 09:29:41 +0000

    add bootstrap no settei

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,15 +3,25 @@
 @section('content')
     
     <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
-    
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-    
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-        {!! Form::label('status', '状況:') !!}
-        {!! Form::text('status') !!}
-        
-        {!! Form::submit('更新') !!}
-        
-    {!! Form::close() !!}
+  <div class="row">
+      <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+  
+            {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+                <div class="form-group">    
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                <div class="form-group">
+                    {!! Form::label('status', '状況:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('更新', null, ['class' => 'btn btn-default']) !!}
+                
+            {!! Form::close() !!}
+          
+      </div>
+  </div>
+
 @endsection
